e56157d97a3986075477067be44a5f0b1dec30af
mwallace98 <mattwallace140@gmail.com>
2023-10-08 18:41:35 -0400

    Project Complete

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import Form from './Form'
+import ToDoList from './TodoList'
+import TodoList from './TodoList'
+
+let id = 0
+let getId = () => ++id
+
+const initialTodos = [
+  {id: getId(),name: 'Walk the Dog', completed: true},
+  {id: getId(),name: 'Learn React', completed: false}
+]
 
 export default class App extends React.Component {
+  state ={
+    todos:initialTodos
+  }
+
+  addTodo = ( name ) => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.concat({id: getId(), completed: false, name})
+    })
+  }
+
+  toggleCompletion = id => {
+    this.setState({
+      ...this.state,
+      todos:this.state.todos.map(td => {
+        if(id == td.id) return {...td, completed: !td.completed}
+        return td
+      })
+    })
+  }
+
   render() {
     return (
       <div>
-        Todo App
+        <TodoList todos={this.state.todos} toggleCompletion={this.toggleCompletion}/>
+        <Form addTodo={this.addTodo}/>
       </div>
     )
   }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import Todo from './Todo'
 
 export default class TodoList extends React.Component {
+  state ={
+    showAll: true
+  }
+
+  toggleShowall = evt => {
+
+    this.setState({
+      ...this.state,
+      showAll: !this.state.showAll
+    })
+  }
   render() {
+    const {todos, toggleCompletion} = this.props
+    const filtered = todos.filter(td => !td.completed || this.state.showAll )
     return (
       <div>
-        TodoList
+        {
+          filtered.map((todo) => (<Todo key={todo.id} todo={todo} toggleCompletion={toggleCompletion}/>))
+        }
+        <button onClick={this.toggleShowall}>{this.state.showAll ? 'Hide completed' : 'Show All'}</button>
       </div>
     )
   }
